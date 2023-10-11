6f4cf845207f5058d43de7b5280c335979988fa4
Gabriel Silva Ezequiel <77858768+Gabrie345@users.noreply.github.com>
2023-10-11 19:30:27 -0300

    start spring io

---
+++ service-funcionarios/src/main/java/com/servicefuncionarios/servicefuncionarios/ServiceFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.servicefuncionarios.servicefuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFuncionariosApplication.class, args);
+	}
+
+}
---
+++ service-funcionarios/src/test/java/com/servicefuncionarios/servicefuncionarios/ServiceFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicefuncionarios.servicefuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
